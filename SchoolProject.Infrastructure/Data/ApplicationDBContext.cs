1c584f4ba68e3fd8702cc84ea0ab31b48348bf5a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-07-25 00:53:37 +0300

    add dbset of entites

---
+++ SchoolProject.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SchoolProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrastructure.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext()
+        {
+            
+        }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmetSubject> DepartmetSubjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+
+    }
+}
