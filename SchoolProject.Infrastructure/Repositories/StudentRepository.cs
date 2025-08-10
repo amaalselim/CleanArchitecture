396f7f08a98742665f58c42733fd6d2cfe4e6d5d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-11 00:32:14 +0300

    implement interface

---
+++ SchoolProject.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities;
+using SchoolProject.Infrastructure.Abstracts;
+using SchoolProject.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrastructure.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        #region Fields
+        private readonly ApplicationDBContext _context;
+        #endregion
+
+        #region Constructors
+        public StudentRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Handles Functions
+        public async Task<List<Student>> GetAllStudentsAsync()
+        {
+            return await _context.Students
+                .ToListAsync();
+        }
+        #endregion
+
+        
+    }
+}
