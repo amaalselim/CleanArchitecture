a0ec8b7ea209700aa789822994e33206273493fc
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-11 20:12:42 +0300

    create dependency of service

---
+++ SchoolProject.Service/ModuleServiceDependencies.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SchoolProject.Infrastructure.Abstracts;
+using SchoolProject.Infrastructure.Repositories;
+using SchoolProject.Service.Abstracts;
+using SchoolProject.Service.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Service
+{
+    public static class ModuleServiceDependencies
+    {
+        public static IServiceCollection AddServiceDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<IStudentService, StudentService>();
+            return services;
+        }
+    }
+}
