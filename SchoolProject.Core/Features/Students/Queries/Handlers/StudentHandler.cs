8c1b5c13b06d163dba7bc104f51716578c699809
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-11 21:13:53 +0300

    create handler of student

---
+++ SchoolProject.Core/Features/Students/Queries/Handlers/StudentHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SchoolProject.Core.Features.Students.Queries.Models;
+using SchoolProject.Data.Entities;
+using SchoolProject.Service.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Core.Features.Students.Queries.Handlers
+{
+    public class StudentHandler : IRequestHandler<GetStudentListQuery, List<Student>>
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentHandler(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+        public async Task<List<Student>> Handle(GetStudentListQuery request, CancellationToken cancellationToken)
+        {
+            return await _studentService.GetAllStudentsAsync();
+        }
+    }
+}
