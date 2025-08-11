d75ca84e9e36cfeedb66536801d0d33a3dee3fbf
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-11 21:26:48 +0300

    create student controller

---
+++ SchoolProject/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Core.Features.Students.Queries.Models;
+
+namespace SchoolProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("List")]
+        public async Task<IActionResult> GetStudentList()
+        {
+            var response = await _mediator.Send(new GetStudentListQuery());
+            return Ok(response);
+        }
+    }
+}
