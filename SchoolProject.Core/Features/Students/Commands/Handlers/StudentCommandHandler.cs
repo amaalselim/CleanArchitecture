cb200d0208867c9b1324f60d716e9039454ca0bc
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-18 00:09:55 +0300

    add regions

---
+++ SchoolProject.Core/Features/Students/Commands/Handlers/StudentCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Core.Features.Students.Commands.Handlers
+{
+    public class StudentCommandHandler
+    {
+        #region Fields
+        #region
+
+        #region Constructor
+        #endregion
+
+        #region handle Functions
+        #endregion
+    }
+}
