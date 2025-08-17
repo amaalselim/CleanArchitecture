d5c4060af4fffcdb13e236bb942eb1a574bb53cb
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-18 00:25:20 +0300

    implement ctor and add field

--- SchoolProject.Core/Features/Students/Commands/Handlers/StudentCommandHandler.cs
+++ SchoolProject.Core/Features/Students/Commands/Handlers/StudentCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using MediatR;
+using SchoolProject.Core.Bases;
+using SchoolProject.Core.Features.Students.Commands.Models;
+using SchoolProject.Service.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +11,27 @@ using System.Threading.Tasks;
 
 namespace SchoolProject.Core.Features.Students.Commands.Handlers
 {
-    public class StudentCommandHandler
+    public class StudentCommandHandler : ResponseHandler,
+                                         IRequestHandler<CreateStudentCommand, Response<string>>
     {
         #region Fields
-        #region
+        private readonly IStudentService _studentService;
+        private readonly IMapper _mapper;
+        #endregion
 
         #region Constructor
+        public StudentCommandHandler(IStudentService studentService,IMapper mapper)
+        {
+            _studentService = studentService;
+            _mapper = mapper;
+        }
         #endregion
 
         #region handle Functions
+        public Task<Response<string>> Handle(CreateStudentCommand request, CancellationToken cancellationToken)
+        {
+            
+        }
         #endregion
     }
 }
