260a185739400c91ed5b99ea5d6822bc50d84da2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-18 01:12:06 +0300

    make student controller inherit frm appcontrollerbase

--- SchoolProject/Controllers/StudentController.cs
+++ SchoolProject/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolProject.API.Base;
 using SchoolProject.Core.Features.Students.Commands.Models;
 using SchoolProject.Core.Features.Students.Queries.Models;
 
@@ -8,32 +9,27 @@ namespace SchoolProject.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StudentController : ControllerBase
+    public class StudentController : AppControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public StudentController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        
         [HttpGet("List")]
         public async Task<IActionResult> GetStudentList()
         {
-            var response = await _mediator.Send(new GetStudentListQuery());
+            var response = await Mediator.Send(new GetStudentListQuery());
             return Ok(response);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetStudentById([FromRoute] int id)
         {
-            var response = await _mediator.Send(new GetStudentByIdQuery(id));
-            return Ok(response);
+            var response = await Mediator.Send(new GetStudentByIdQuery(id));
+            return NewResult(response);
         }
 
         [HttpPost("Create")]
         public async Task<IActionResult> Create([FromBody] CreateStudentCommand command)
         {
-            var response = await _mediator.Send(command);
-            return Ok(response);
+            var response = await Mediator.Send(command);
+            return NewResult(response);
         }
 
     }
