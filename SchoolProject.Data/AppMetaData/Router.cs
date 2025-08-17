7ae07830118d0928fdc92a8748f19101a4abfbf2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-17 23:13:42 +0300

    make class of routing

---
+++ SchoolProject.Data/AppMetaData/Router.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.AppMetaData
+{
+    public static class Router
+    {
+        public const string root = "api";
+        public const string rule = root + "/";
+
+        public static class StudentRouting
+        {
+            public const string Prefix = rule + "student";
+            public const string List = Prefix + "/list";
+            public const string GetById = Prefix + "/{id}";
+
+        }
+    }
+}
