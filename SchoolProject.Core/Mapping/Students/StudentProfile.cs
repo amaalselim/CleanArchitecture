c21eba6ead660a8a952814fc543e926a46b15cb9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-17 22:55:44 +0300

    add method mapping to profile

--- SchoolProject.Core/Mapping/Students/StudentProfile.cs
+++ SchoolProject.Core/Mapping/Students/StudentProfile.cs
@@ -14,6 +14,7 @@ namespace SchoolProject.Core.Mapping.Students
         public StudentProfile()
         {
             GetStudentListMapping();
+            GetStudentByIdMapping();
         }
     }
 }
