6275c464b7e1f0cd5ae1e099278c702d0a7d33d4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-18 00:45:32 +0300

    add createstudentmapper

---
+++ SchoolProject.Core/Mapping/Students/CommandMapping/CreateStudentCommandMapping.cs
@@ -0,0 +1,19 @@
+﻿using SchoolProject.Core.Features.Students.Commands.Models;
+using SchoolProject.Core.Features.Students.Queries.Results;
+using SchoolProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SchoolProject.Core.Mapping.Students
+{
+    public partial class StudentProfile
+    {
+        public void CreateStudentCommandMapping()
+        {
+            CreateMap<CreateStudentCommand,Student>()
+                .ForMember(dest => dest.DID, opt => opt.MapFrom(src => src.DepartmentId));
+        }
+    }
+}
