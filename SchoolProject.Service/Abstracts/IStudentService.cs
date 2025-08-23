3a834492738e41e2fb22cd5a0ea5670ed293b8c4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-23 23:36:50 +0300

    add IsNameExist interface

--- SchoolProject.Service/Abstracts/IStudentService.cs
+++ SchoolProject.Service/Abstracts/IStudentService.cs
@@ -1,9 +1,4 @@
 ﻿using SchoolProject.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SchoolProject.Service.Abstracts
 {
@@ -12,5 +7,6 @@ namespace SchoolProject.Service.Abstracts
         public Task<List<Student>> GetAllStudentsAsync();
         public Task<Student> GetStudentByIdAsync(int id);
         public Task<string> AddAsync(Student student);
+        public Task<bool> IsNameExist(string name);
     }
 }
