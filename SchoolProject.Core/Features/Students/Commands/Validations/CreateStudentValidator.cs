2b5632e6f615e3e552be2a53bba3dfb536d9e315
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-23 22:57:40 +0300

    add rules of attributes

--- SchoolProject.Core/Features/Students/Commands/Validations/CreateStudentValidator.cs
+++ SchoolProject.Core/Features/Students/Commands/Validations/CreateStudentValidator.cs
@@ -9,10 +9,27 @@ namespace SchoolProject.Core.Features.Students.Commands.Validations
         #endregion
 
         #region Constructors
-        public CreateStudentValidator() { }
+        public CreateStudentValidator()
+        {
+            ApplyValidationsRules();
+        }
         #endregion
 
         #region Handle Functions
+        public void ApplyValidationsRules()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name Must Not Be Empty")
+                .NotNull().WithMessage("Name Must Not Be Null")
+                .MaximumLength(00).WithMessage("Name cannot exceed 10 characters");
+
+            RuleFor(x => x.Address)
+                .NotEmpty().WithMessage("{Property Name} Must Not Be Empty")
+                .NotNull().WithMessage("{Property Value} Must Not Be Null")
+                .MaximumLength(00).WithMessage("{Property Name} cannot exceed 10 characters");
+
+
+        }
         #endregion
 
     }
