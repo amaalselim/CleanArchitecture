a6f1c16f93a51e97dec156ab317e4ac22889bc34
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-23 22:48:45 +0300

    add createstudent validator

---
+++ SchoolProject.Core/Features/Students/Commands/Validations/CreateStudentValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SchoolProject.Core.Features.Students.Commands.Models;
+
+namespace SchoolProject.Core.Features.Students.Commands.Validations
+{
+    public class CreateStudentValidator : AbstractValidator<CreateStudentCommand>
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public CreateStudentValidator() { }
+        #endregion
+
+        #region Handle Functions
+        #endregion
+
+    }
+}
