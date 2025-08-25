2580737aba47e5dd0d2bc30cb0fff404a562fd22
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-25 23:54:14 +0300

    create delete async interface

--- SchoolProject.Service/Abstracts/IStudentService.cs
+++ SchoolProject.Service/Abstracts/IStudentService.cs
@@ -8,6 +8,7 @@ namespace SchoolProject.Service.Abstracts
         public Task<Student> GetStudentByIdAsync(int id);
         public Task<string> AddAsync(Student student);
         public Task<string> EditAsync(Student student);
+        public Task<string> DeleteAsync(int id);
         public Task<bool> IsNameExist(string name);
         public Task<bool> IsNameExistExcludeSelf(string name, int id);
     }
