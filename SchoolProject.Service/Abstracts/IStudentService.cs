47b53e6ccb908de1ca33bd3c93d53bbeb2545111
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-25 23:06:29 +0300

    add interface of action edit async

--- SchoolProject.Service/Abstracts/IStudentService.cs
+++ SchoolProject.Service/Abstracts/IStudentService.cs
@@ -7,6 +7,7 @@ namespace SchoolProject.Service.Abstracts
         public Task<List<Student>> GetAllStudentsAsync();
         public Task<Student> GetStudentByIdAsync(int id);
         public Task<string> AddAsync(Student student);
+        public Task<string> EditAsync(Student student);
         public Task<bool> IsNameExist(string name);
         public Task<bool> IsNameExistExcludeSelf(string name, int id);
     }
