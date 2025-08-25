f3c652b5d0479014778965e0a7518a8d58643c89
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-25 22:45:33 +0300

    edit message

--- SchoolProject.Core/Features/Students/Commands/Validations/CreateStudentValidator.cs
+++ SchoolProject.Core/Features/Students/Commands/Validations/CreateStudentValidator.cs
@@ -26,12 +26,12 @@ namespace SchoolProject.Core.Features.Students.Commands.Validations
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Name Must Not Be Empty")
                 .NotNull().WithMessage("Name Must Not Be Null")
-                .MaximumLength(100).WithMessage("Name cannot exceed 10 characters");
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters");
 
             RuleFor(x => x.Address)
                 .NotEmpty().WithMessage("{PropertyName} Must Not Be Empty")
                 .NotNull().WithMessage("{PropertyValue} Must Not Be Null")
-                .MaximumLength(100).WithMessage("{PropertyName} cannot exceed 10 characters");
+                .MaximumLength(100).WithMessage("{PropertyName} cannot exceed 100 characters");
 
 
         }
