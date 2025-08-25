5e8107d0f8ed56fc3f98ce1c170fd4cdb21f3391
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-25 22:31:01 +0300

    add editclass command

---
+++ SchoolProject.Core/Features/Students/Commands/Models/EditStudentCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SchoolProject.Core.Bases;
+
+namespace SchoolProject.Core.Features.Students.Commands.Models
+{
+    public class EditStudentCommand : IRequest<Response<string>>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
