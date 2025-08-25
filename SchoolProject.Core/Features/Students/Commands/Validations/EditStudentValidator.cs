922036b1df88ed903d3f2505123ac840a55b1c5e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-08-25 22:34:57 +0300

    create edit student validator

---
+++ SchoolProject.Core/Features/Students/Commands/Validations/EditStudentValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+using SchoolProject.Core.Features.Students.Commands.Models;
+
+namespace SchoolProject.Core.Features.Students.Commands.Validations
+{
+    public class EditStudentValidator : AbstractValidator<EditStudentCommand>
+    {
+    }
+}
